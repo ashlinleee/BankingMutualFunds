6563662fe7ffef7aff522bce9412214d00b1f690
PremThakareitm <2023.premt@isu.ac.in>
2026-05-04 13:31:53 +0530

    Changes in Application

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mfRoutes = require('./routes/mf');
+const cbsRoutes = require('./routes/cbs');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/mf', mfRoutes);
+app.use('/api/cbs', cbsRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
